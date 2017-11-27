3de2d8ed5db45378fc383020b789e147db6dd157
RuslanBarcho <RRCFoxyLoxy@yamdex.ru>
2017-11-28 01:02:27 +0300

    added all missing project files

---
+++ app/src/main/java/radonsoft/firenotes/MainActivity.java
@@ -0,0 +1,51 @@
+package radonsoft.firenotes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import radonsoft.firenotes.Fragments.NoteFragment;
+import radonsoft.firenotes.Fragments.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity {
+    NoteFragment notes = new NoteFragment();
+    FragmentTransaction ftrans = getSupportFragmentManager().beginTransaction();
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction navtrans = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_notes:
+                    navtrans.replace(R.id.content, new NoteFragment()).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    navtrans.replace(R.id.content, new SettingsFragment()).commit();
+                    return true;
+            }
+
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ftrans.replace(R.id.content, new NoteFragment());
+        ftrans.commit();
+    }
+
+}
